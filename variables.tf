6fd613b1c4d8b27ba9e92f264c14e1608c181b3d
mranumolu <45801573+mranumolu@users.noreply.github.com>
2019-10-04 17:27:29 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "resource_group_name" {
 
 variable "location" {
   description = "The location/region where the core network will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
-  dedault     = "centralus"
+  default     = "centralus"
   
 }
 
